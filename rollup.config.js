57f9993625c36c40673d9107511c8d33f201cb2d
Jon <jon@dbs.org>
2024-06-06 02:26:05 -0500

    Moved to just .js

--- rollup.config.js
+++ rollup.config.js
@@ -3,12 +3,9 @@ import { terser } from 'rollup-plugin-terser';
 export default {
   input: './src/FuzzyTable.js',
   output: {
-    file: './dist/FuzzyTable.mjs',
-    format: 'esm',
-    name: 'FuzzyTable', // This will attach `FuzzyTable` to the `window` object in browsers
-    globals: {
-      FuzzyTable: 'FuzzyTable'
-    }
+    file: 'dist/FuzzyTable.js',
+    format: 'umd',  // UMD format for universal compatibility
+    name: 'FuzzyTable'
   },
   plugins: [
     terser()
